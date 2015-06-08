2b428b209d637d20e8a0a933ac51d8fd40e90ca5
Tim Shimmin <timothy.shimmin.10+github@cnu.edu>
2015-06-07 23:20:50 -0400

    Creates RAR with updated library!
    Modified lib-rar-python to work for me. I converted skozacik's library
    to Python 3, then fixed its 2to3 automated-conversion problems. Linked
    below.
    
    Now creates RAR archive. Unfortunately fills it with the whole folder
    structure from root.
    
    https://github.com/TimothyShimmin/lib-rar-python

--- Unmarkt.py
+++ Unmarkt.py
@@ -12,15 +12,13 @@ os.chdir(testdir)  # make it the active directory
 comic = 'spider-man.cbr'    # name of the comic
 
 
-
 # region Extract from cbr
 extractRar = rarfile.RarFile(comic)     # set existing RAR to this variable
-extractDirname = testdir + comic.replace('.cbr', '')
-print("Extracting here: " + extractDirname)
-extractRar.extractall(extractDirname)
+extractDir = testdir + comic.replace('.cbr', '')
+print("Extracting here: " + extractDir)
+extractRar.extractall(extractDir)
 extractRar.close()
 # endregion
-
 
 
 # Write to zip file
@@ -31,21 +29,24 @@ if os.path.isfile('written ' + comic):
 
 # writeRar = archive.Archive('written ' + comic)      # create rar object to write to
 # print(os.getcwd())
-# writeRar = archive.Archive(os.getcwd() + '\\written ' + comic)      # create rar object to write to
-writeRar = archive.Archive('D:\\Users\\Kat\\Documents\\GitHub\\Unmarkt\\test\\written test.rar')      # create rar object to write to
+# writeRar = archive.Archive(os.getcwd() + '\\written ' + comic)      # create cbr object to write to
+# writeRar = archive.Archive('D:\\Users\\Kat\\Documents\\GitHub\\Unmarkt\\test\\written test.rar')      # create rar object to write to
+
+# base = "D:\\Users\\Kat\\Documents\\GitHub\\Unmarkt\\test"
+# writeRar = archive.Archive("written test.rar", base)      # create rar object to write to
+writeRar = archive.Archive("written test.rar", testdir)      # create rar object to write to
 
 
 
 
-os.chdir(extractDirname)                        # TODO: stay in parent folder? minimize switching folders?
+os.chdir(extractDir)                        # TODO: stay in parent folder? minimize switching folders?
 filenames = [f for f in os.listdir('.') if os.path.isfile(f)]
 print(filenames)
 for filename in filenames:
     # TODO: Delete correct image
     print(filename)
-    # writeZip.write(filename, compress_type=zipfile.ZIP_DEFLATED)
-    # writeRar.write(filename, compress_type=zipfile.ZIP_DEFLATED)
-    writeRar.add_file(filename)     # TODO: confirm translation to librar.0.0.4 functions and arguments
+    # writeRar.add_file(filename)     # TODO: confirm translation to librar.0.0.4 functions and arguments
+writeRar.add_dir(extractDir)     # TODO: confirm translation to librar.0.0.4 functions and arguments
 
 
 # writeZip.close()
@@ -53,11 +54,10 @@ writeRar.run()
 
 
 # Delete extraction directory
-# os.chdir(testdir)
+os.chdir(testdir)
 # print("Current working directory: " + os.getcwd())
-# shutil.rmtree(extractDirname)
+shutil.rmtree(extractDir)
 
 
 
 # TODO: Use OOP ?
-
