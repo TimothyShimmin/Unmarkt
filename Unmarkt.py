9b754c89b7f1ff37034b0266663a03c4d599e9cb
Tim Shimmin <timothy.shimmin.10+github@cnu.edu>
2016-02-22 19:29:56 -0500

    Merge this with CBZ-Functionality?
    I did this at some point, not sure where to put it.

--- Unmarkt.py
+++ Unmarkt.py
@@ -2,39 +2,54 @@
 # Unmarkt.py - Removes release group tags from digital comic books (cbr).
 __author__ = 'Tim Shimmin'
 
-import os, shutil, rarfile
+import os, shutil, zipfile, rarfile
 from librar import archive
+from enum import Enum
+
+# archiveType = Enum('cbz', 'cbr')
+class archiveType(Enum):
+    cbz = 1
+    cbr = 2
 
 # initialize directory
 testdir = 'D:\\Users\\Kat\\Documents\\GitHub\\Unmarkt\\test\\'
 os.chdir(testdir)  # make it the active directory
 
 comic = 'spider-man.cbr'    # name of the comic
+comicType = archiveType.cbz # assuming cbz until notified if CBR
 
+if comic.endswith(".cbr"):
+    comicType = archiveType.cbr
 
+# region Extract from cbz
+if comicType == archiveType.cbz:
+    extractZip = zipfile.ZipFile(comic)     # set existing ZIP to this variable
+    extractDirname = testdir + comic.replace('.cbz', '')
+    print("Extracting ZIP here: " + extractDirname)
+    extractZip.extractall(extractDirname)
+    extractZip.close()
+# endregion
 
 # region Extract from cbr
-extractRar = rarfile.RarFile(comic)     # set existing RAR to this variable
-extractDirname = testdir + comic.replace('.cbr', '')
-print("Extracting here: " + extractDirname)
-extractRar.extractall(extractDirname)
-extractRar.close()
+if comicType == archiveType.cbr:
+    extractRar = rarfile.RarFile(comic)     # set existing RAR to this variable
+    extractDirname = testdir + comic.replace('.cbr', '')
+    print("Extracting RAR here: " + extractDirname)
+    extractRar.extractall(extractDirname)
+    extractRar.close()
+    comic = comic.replace('.cbr', '.cbz')
 # endregion
 
 
 
-# Write to zip file
-# TODO: Write to cbr
+
+# region Write to zip file
 os.chdir(testdir)  # make it the active directory
 if os.path.isfile('written ' + comic):
     os.remove('written ' + comic)
 
-# writeRar = archive.Archive('written ' + comic)      # create rar object to write to
+writeZip = zipfile.ZipFile('written ' + comic, 'a')      # 'a' for append to current
 # print(os.getcwd())
-# writeRar = archive.Archive(os.getcwd() + '\\written ' + comic)      # create rar object to write to
-writeRar = archive.Archive('D:\\Users\\Kat\\Documents\\GitHub\\Unmarkt\\test\\written test.rar')      # create rar object to write to
-
-
 
 
 os.chdir(extractDirname)                        # TODO: stay in parent folder? minimize switching folders?
@@ -43,21 +58,21 @@ print(filenames)
 for filename in filenames:
     # TODO: Delete correct image
     print(filename)
-    # writeZip.write(filename, compress_type=zipfile.ZIP_DEFLATED)
+    writeZip.write(filename, compress_type=zipfile.ZIP_DEFLATED)
     # writeRar.write(filename, compress_type=zipfile.ZIP_DEFLATED)
-    writeRar.add_file(filename)     # TODO: confirm translation to librar.0.0.4 functions and arguments
+    # writeRar.add_file(filename)     # TODO: confirm translation to librar.0.0.4 functions and arguments
 
 
-# writeZip.close()
-writeRar.run()
-
+writeZip.close()
+# writeRar.run()
+# endregion
 
 # Delete extraction directory
-# os.chdir(testdir)
+os.chdir(testdir)
 # print("Current working directory: " + os.getcwd())
 # shutil.rmtree(extractDirname)
 
 
 
-# TODO: Use OOP ?
+# TODO: Use OOP ? Useful with batch?
 
