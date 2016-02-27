16b8ffddd7ea5d5ea3e37e38195e8abe281afe4d
Tim Shimmin <timothy.shimmin.10+github@cnu.edu>
2016-02-26 21:37:28 -0500

    Folder before images solution; os.walk
    Using os.walk to go through the folder. If the images are in a
    subfolder, changes current working directory to that folder. Surely
    there's a better way to do this, but it works for now. Then for each
    file (image) in the current directory, if it passes the filter, it is
    written to the zip. Commented out the old solution.

--- Unmarkt.py
+++ Unmarkt.py
@@ -1,7 +1,6 @@
 #! python3
 # Unmarkt.py - Removes release group tags from digital comic books (cbr).
 __author__ = 'Tim Shimmin'
-# TODO: START @ LINE 103 --- Pull images from folder-within-archive instead of just the archive --- os.walk?
 # TODO: Move TODOs to GitHub issues?
 # TODO: Move to separate methods
 # TODO: Move new comic to different directory, leaving original untouched in case of mistakes --- option in settings?
@@ -19,15 +18,12 @@ class archiveType(Enum):
     cbr = 3
 
 # region # initialize directory
-# print(os.path.dirname(os.path.realpath(__file__)))
 testdir = os.path.dirname(os.path.realpath(__file__)) + '\\test\\'
 print(testdir)
-# print(os.listdir(testdir))
 os.chdir(testdir)  # make it the active directory
 # endregion
 
 # region Comic filename
-# testdir = 'C:\\Users\\Tim.Shimmin\\Documents\\GitHub\\Unmarkt\\test2\\'
 comic = 'spider-man.cbz'  # name of the comic
 # comic = 'spider-man (2).cbz'  # without folder in archive ---name of the comic
 # endregion
@@ -96,18 +92,27 @@ writeZip = zipfile.ZipFile('written ' + comic, 'a')      # 'a' for append to cur
 os.chdir(extractDir)                        # TODO: stay in parent folder?
 # directories = [d for d in os.walk('.') if os.path.isfile(d)]
 # print(directories)
-for files in os.walk('.'):
-    print(files)
+for root, dirs, files in os.walk('.'):
+    print(dirs)
+    if dirs:
+        if dirs[0] != '':
+            os.chdir(dirs[0])
+            print(dirs[0])
+for root, dirs, files in os.walk('.'):
+    for file in files:
+        if file.find('zzz') == -1:        # TODO: Parsing method that checks all different kinds of rls groups.
+            writeZip.write(file, compress_type=zipfile.ZIP_DEFLATED)
+            print(file)
     # print(files[1][0])
     # print(os.walk('.')[2][0])
     # if '.\\'.join(files[1]) == files[0]:        # TODO: files[1] is from first 'files' in os.walk, files[0] is from second 'files' in os.walk
         # TODO: take files[2] for new archive                  # files[2] is from second 'files' in os.walk
-filenames = [f for f in os.listdir('.') if os.path.isfile(f[2])]
-print(filenames)
-for filename in filenames:
-    if filename.find('zzz') == -1:        # TODO: Parsing method that checks all different kinds of rls groups.
-        writeZip.write(filename, compress_type=zipfile.ZIP_DEFLATED)
-        print(filename)
+# filenames = [f for f in os.listdir('.') if os.path.isfile(f[2])]
+# print(filenames)
+# for filename in filenames:
+#     if filename.find('zzz') == -1:        # TODO: Parsing method that checks all different kinds of rls groups.
+#         writeZip.write(filename, compress_type=zipfile.ZIP_DEFLATED)
+#         print(filename)
 writeZip.close()
 
 # region # Rename zip to cbz     --- not sure how this works anymore but it might be useful for cbr
